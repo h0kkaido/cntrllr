ad0a082e1ce86e6650d3177048a2b0b27fc40127
Иван Жестков <h0kkaido@MBP-Ivan.2gis.local>
2020-03-27 11:50:35 +0300

    up

--- activatr.js
+++ activatr.js
@@ -9,7 +9,7 @@ const YouTube = require('youtube-live-chat');
 var key = "AIzaSyDqKu4zR00WE46RJHpqPHy4R5Z50qXsJcc";
 var key2 = "AIzaSyB-66pR-gHc228jOCc0T_vtKIy6v8A-I68";
 var key3 = "AIzaSyAmXdrozCnZ2flmKXRJyDpxyDXANzyDvOo";
-var channel = "UCqJ5zFEED1hWs0KNQCQuYdQ";
+var channel = "UC5c9VlYTSvBSCaoMu_GI6gQ";
 var parsetimeout = 2500;
 var shorttimeout = 2000;
 var timeout = 4000;
@@ -25,37 +25,37 @@ yt.on('ready', () => {
 })
 
 yt.on('message', data => {
-    if (data.snippet.displayMessage == "f") {
+    if (data.snippet.displayMessage == "ВПЕРЕД") {
         console.log("[W]")
         ks.startBatch()
-        .batchTypeKey('W', timeout)
-        .sendBatch();
+            .batchTypeKey('W', timeout)
+            .sendBatch();
     }
-    if (data.snippet.displayMessage == "wow") {
+    if (data.snippet.displayMessage == "НАЛЕВО") {
         console.log("[A]")
         ks.startBatch()
-        .batchTypeKey('W', shorttimeout)
-        .batchTypeKey('A', timeout)
-        .sendBatch();
+            .batchTypeKey('W', shorttimeout)
+            .batchTypeKey('A', timeout)
+            .sendBatch();
     }
-    if (data.snippet.displayMessage == "hola") {
+    if (data.snippet.displayMessage == "НАЗАД") {
         console.log("[S]")
         ks.startBatch()
-        .batchTypeKey('S', timeout)
-        .sendBatch();
+            .batchTypeKey('S', timeout)
+            .sendBatch();
     }
-    if (data.snippet.displayMessage == "XD") {
+    if (data.snippet.displayMessage == "НАПРАВО") {
         console.log("[D]")
         ks.startBatch()
-        .batchTypeKey('W', shorttimeout)
-        .batchTypeKey('D', timeout)
-        .sendBatch();
+            .batchTypeKey('W', shorttimeout)
+            .batchTypeKey('D', timeout)
+            .sendBatch();
     }
-    if (data.snippet.displayMessage == "gona") {
+    if (data.snippet.displayMessage == "СТОП") {
         console.log("[SPACE]")
         ks.startBatch()
-        .batchTypeKey('space', timeout)
-        .sendBatch();
+            .batchTypeKey('space', timeout)
+            .sendBatch();
     }
 })
 
