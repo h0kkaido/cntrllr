e84c98483835ae4a502bd18c9d488513b59ca9be
ivanzhestkov99 <ivanzhestkov99@gmail.com>
2020-03-29 02:34:58 +0300

    v1

--- activatr.js
+++ activatr.js
@@ -3,59 +3,38 @@
 var ks = require('node-key-sender');
 ks.setOption('globalDelayPressMillisec', 2000);
 const YouTube = require('youtube-live-chat');
-
-/* CONFIG */
-
-var key = "AIzaSyDqKu4zR00WE46RJHpqPHy4R5Z50qXsJcc";
-var key2 = "AIzaSyB-66pR-gHc228jOCc0T_vtKIy6v8A-I68";
-var key3 = "AIzaSyAmXdrozCnZ2flmKXRJyDpxyDXANzyDvOo";
-var channel = "UC5c9VlYTSvBSCaoMu_GI6gQ";
-var parsetimeout = 2500;
-var shorttimeout = 2000;
-var timeout = 4000;
-var longtimeout = 8000;
+const config = require('./config.json');
 
 /* CODE */
 
-const yt = new YouTube(channel, key);
+const yt = new YouTube(config.channel, config.key2);
 
 yt.on('ready', () => {
     console.log("Start!")
-    yt.listen(parsetimeout)
+    yt.listen(config.parsetimeout)
 })
 
 yt.on('message', data => {
-    if (data.snippet.displayMessage == "ВПЕРЕД") {
+    console.log(data.snippet.displayMessage)
+    if (data.snippet.displayMessage == "ВПЕРЁД") {
         console.log("[W]")
-        ks.startBatch()
-            .batchTypeKey('W', timeout)
-            .sendBatch();
-    }
-    if (data.snippet.displayMessage == "НАЛЕВО") {
-        console.log("[A]")
-        ks.startBatch()
-            .batchTypeKey('W', shorttimeout)
-            .batchTypeKey('A', timeout)
-            .sendBatch();
-    }
-    if (data.snippet.displayMessage == "НАЗАД") {
-        console.log("[S]")
-        ks.startBatch()
-            .batchTypeKey('S', timeout)
-            .sendBatch();
-    }
-    if (data.snippet.displayMessage == "НАПРАВО") {
-        console.log("[D]")
-        ks.startBatch()
-            .batchTypeKey('W', shorttimeout)
-            .batchTypeKey('D', timeout)
-            .sendBatch();
+        ks.sendKey('w');
     }
     if (data.snippet.displayMessage == "СТОП") {
         console.log("[SPACE]")
-        ks.startBatch()
-            .batchTypeKey('space', timeout)
-            .sendBatch();
+        ks.sendKey('space');
+    }
+    if (data.snippet.displayMessage == "НАЗАД") {
+        console.log("[S]")
+        ks.sendKey('s');
+    }
+    if (data.snippet.displayMessage == "НАЛЕВО") {
+        console.log("[A]")
+        ks.sendKey('a');
+    }
+    if (data.snippet.displayMessage == "НАПРАВО") {
+        console.log("[D]")
+        ks.sendKey('d');
     }
 })
 
